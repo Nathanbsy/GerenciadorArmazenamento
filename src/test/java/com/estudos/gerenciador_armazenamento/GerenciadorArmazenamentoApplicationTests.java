76386a23712ef4baa8887c14ab5980b47ea101c1
Nathanbsy <nathanbs1227@gmail.com>
2026-04-20 19:42:00 -0300

    Iniciando o projeto

---
+++ src/test/java/com/estudos/gerenciador_armazenamento/GerenciadorArmazenamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.gerenciador_armazenamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorArmazenamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
